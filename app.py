dc229f62b8922b62ea015f007d28663976382c97
mikeybow <micbowman25@gmail.com>
2021-09-20 14:42:50 -0500

    studying if elif and else

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-print("Hello World")
+good_credit = False
+bad_credit = True
+
+if good_credit:
+    print('You will need to make a down payment of 10%')
+elif bad_credit:
+    print("Sorry we won't be able to accept a down payment")
+else:
+    print('You will need to make a down payment of 20%')
